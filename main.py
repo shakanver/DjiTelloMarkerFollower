5eeb2c5bb0b13cf4848db20fad76ec8748bb8fb7
shakeel anver <shakeelanver26@gmail.com>
2025-04-26 23:31:26 +1000

    added video streaming functionality

--- main.py
+++ main.py
@@ -2,23 +2,35 @@
 Entry Point
 """
 
-import threading
-import video_service
-import flight_service
 import cv2
+import flight_service
+import os
+import threading
 import time
+import video_service
 
 from djitellopy import Tello
 from cache import Cache
+from datetime import datetime
 
 # TODO; find a way to gracefully handle keyboard interrupts.
 
 FLIGHT_TIME = 60 #TODO: make this a command line argument and set default flight time to 60
 cache = Cache()
 
+"""Create a Tello instance, connect to it and enable video streaming services"""
 tello = Tello()
 tello.connect()
 tello.streamon()
+
+"""Create a video writer so that video stream can be saved to file for debugging and analysis purposes"""
+frame_width = 960
+frame_height = 720
+recordings_dir = './recordings'
+if not os.path.exists(recordings_dir):
+	os.makedirs(recordings_dir)
+
+video_out = cv2.VideoWriter(f'recordings/output_{datetime.now().strftime("%d%m%Y_%H%M%S")}.mp4', cv2.VideoWriter_fourcc(*'XVID'), 20.0, (frame_width, frame_height))
 
 try:
 	stop_event = threading.Event()
@@ -29,11 +41,13 @@ try:
 	video_thread.start()
 	flight_thread.start()
 
+	"""cv2 requires that only the main thread streams images and videos."""
 	start_time = time.time()
 	while time.time() - start_time < FLIGHT_TIME:
 		frame = cache.get_frame()
 		if frame is not None:
 			cv2.imshow("Frame", frame)
+			video_out.write(cv2.resize(frame, (frame_width, frame_height)))
 
 		if cv2.waitKey(1) and 0xFF == ord('q'):
 			break
@@ -48,6 +62,7 @@ try:
 		raise Exception("One of the service threads failed to stop, force quitting")
 	
 finally:
+	video_out.release()
 	tello.streamoff()
 	tello.end()
 
